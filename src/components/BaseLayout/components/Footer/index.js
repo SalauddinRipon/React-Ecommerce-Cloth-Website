865160df65b715a2b64017419b134246da5ff6e0
SalauddinRipon <salauddinripon85@gmail.com>
2019-08-20 04:05:27 +0600

    finised project

---
+++ src/components/BaseLayout/components/Footer/index.js
@@ -0,0 +1,12 @@
+//Dependencies
+import React from 'react';
+//Internals
+import './index.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p>© 2019 Copyright Salauddin  Ripon</p>
+  </div>
+)
+
+export default Footer;
